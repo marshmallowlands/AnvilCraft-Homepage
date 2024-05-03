66a49a739d88539bee0b85e70b8bae82599b5c7f
Cjsah <cjsah@cjsah.com>
2024-05-03 13:58:02 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,33 @@
-import {createRouter, createWebHistory, Router} from 'vue-router';
-import MainPage from "../views/MainPage.vue";
-import ImageViewPage from "../views/ImageViewPage.vue";
-import AboutPage from "../views/AboutPage.vue";
-import SupportPage from "../views/SupportPage.vue";
+import { createRouter, createWebHashHistory, Router } from 'vue-router';
+import MainPage from '../views/MainPage.vue';
+import ImageViewPage from '../views/ImageViewPage.vue';
+import AboutPage from '../views/AboutPage.vue';
+import SupportPage from '../views/SupportPage.vue';
 
 const router: Router = createRouter({
-    history: createWebHistory('/web'),
-    routes: [
-        {
-            path: '/',
-            name: 'Main',
-            component: MainPage
-        },
-        {
-            path: '/view',
-            name: 'Image',
-            component: ImageViewPage
-        },
-        {
-            path: '/about',
-            name: 'About',
-            component: AboutPage
-        },
-        {
-            path: '/support',
-            name: 'Support',
-            component: SupportPage
-        }
-    ]
+  history: createWebHashHistory('/web'),
+  routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: MainPage
+    },
+    {
+      path: '/view',
+      name: 'Image',
+      component: ImageViewPage
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: AboutPage
+    },
+    {
+      path: '/support',
+      name: 'Support',
+      component: SupportPage
+    }
+  ]
 });
 
 export default router;
